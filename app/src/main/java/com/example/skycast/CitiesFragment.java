502fc8fa3c36e239d4fc81f93410fd7f03520c68
Boules02010458 <angelos.boules24@gmail.com>
2024-11-16 21:08:41 -0500

    Added basic outline for CitiesFragment and the layout file

--- app/src/main/java/com/example/skycast/CitiesFragment.java
+++ app/src/main/java/com/example/skycast/CitiesFragment.java
@@ -8,14 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.skycast.R;
-
-public class HomeFragment extends Fragment {
+public class CitiesFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_cities, container, false);
     }
 }
