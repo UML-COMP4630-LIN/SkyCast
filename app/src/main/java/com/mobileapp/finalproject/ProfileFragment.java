663c71cf143b076b9444113f67b6e14eac1f1754
theodorfarag <Theodorfarag@gmail.com>
2024-11-19 11:44:55 -0500

    added profilr fragment and basic changed on it so far

---
+++ app/src/main/java/com/mobileapp/finalproject/ProfileFragment.java
@@ -0,0 +1,27 @@
+package com.mobileapp.finalproject;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.skycast.databinding.FragmentProfileBinding;
+
+
+public class ProfileFragment extends Fragment {
+    private FragmentProfileBinding binding;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+}
